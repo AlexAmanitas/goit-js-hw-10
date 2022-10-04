1dac7f990ffdd1bf008c0be4c332de896c106c7c
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-10-04 22:29:53 +0300

    arrow problem solved

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import weather from './templates/weather.hbs';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { fetchWeather } from './js/fetchWeather';
+import arrow from './images/icons8-up-50.png';
 
 const DEBOUNCE_DELAY = 300;
 const refs = {
@@ -124,7 +125,7 @@ function weatherInfo(city) {
       icon: `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.weather[0].icon}.svg`,
       windDeg: data.wind.deg,
       windSpeed: data.wind.speed,
-      windArrow: './images/icons8-up-50.png',
+      windArrow: arrow,
     };
 
     weatherRender(weatherObj);
